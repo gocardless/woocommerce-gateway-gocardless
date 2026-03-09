33dbd778259aac5df12bf62895a094f2a9011a4e
Sumit Bagthariya <67687255+qasumitbagthariya@users.noreply.github.com>
2026-03-09 14:43:40 +0530

    Compatibility testing with Woo 10.6 Beta

--- woocommerce-gateway-gocardless.php
+++ woocommerce-gateway-gocardless.php
@@ -12,8 +12,8 @@
  * License:              GPL-3.0-or-later
  * License URI:          https://spdx.org/licenses/GPL-3.0-or-later.html
  * Requires Plugins:     woocommerce
- * WC requires at least: 10.2
- * WC tested up to:      10.4
+ * WC requires at least: 10.4
+ * WC tested up to:      10.6
  *
  * Copyright: © 2023-2025 WooCommerce
  * Copyright: © 2025 GoCardless
