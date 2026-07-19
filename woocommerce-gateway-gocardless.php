185a55e844434455b293fe355cf3bcee5c8d5ccc
Sumit Bagthariya <67687255+qasumitbagthariya@users.noreply.github.com>
2026-07-19 16:04:41 +0530

    Compatibility testing with WC 11 beta 1

--- woocommerce-gateway-gocardless.php
+++ woocommerce-gateway-gocardless.php
@@ -4,7 +4,7 @@
  * Plugin URI:           https://www.woocommerce.com/products/gocardless/
  * Description:          Extends both WooCommerce and WooCommerce Subscriptions with the GoCardless Payment Gateway. A GoCardless merchant account is required.
  * Version:              3.0.0
- * Requires at least:    6.8
+ * Requires at least:    6.9
  * Requires PHP:         7.4
  * PHP tested up to:     8.3
  * Author:               GoCardless
@@ -12,8 +12,8 @@
  * License:              GPL-3.0-or-later
  * License URI:          https://spdx.org/licenses/GPL-3.0-or-later.html
  * Requires Plugins:     woocommerce
- * WC requires at least: 10.7
- * WC tested up to:      10.9
+ * WC requires at least: 10.8
+ * WC tested up to:      11.0
  *
  * Copyright: © 2023-2025 WooCommerce
  * Copyright: © 2025 GoCardless
